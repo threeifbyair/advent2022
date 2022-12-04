cf74143eabb841e9c67a676311d0f23be972b593
Alistair Bell <alistair@bellsonline.com>
2022-12-03 22:36:39 -0500

    Day 3 part 2.

--- src/bin/3.rs
+++ src/bin/3.rs
@@ -1,15 +1,18 @@
 use advent2022::Support;
 use std::io;
-use std::collections::HashSet;
+use std::collections::hash_set::HashSet;
 
 fn main() -> io::Result <()> {    
     let sup = Support::new()?;
 
     let mut score: u32 = 0;
+    let mut nelves: u32 = 0;
+    let mut dataset: HashSet<char> = HashSet::new();
+    let mut prevdataset: HashSet<char> = HashSet::new();
+    let mut prevprevdataset: HashSet<char> = HashSet::new();
     for line in sup.lines {
         let innerline = line?;
         let linelen = innerline.len();
-        let mut dataset = HashSet::new();
         for ch in innerline[0..linelen/2].chars() {
             dataset.insert(ch);
         }
@@ -17,15 +20,46 @@ fn main() -> io::Result <()> {
         for ch in innerline[linelen/2..linelen].chars() {
             if dataset.contains(&ch) {
                 priochr = ch;
-                break;
             }
+            dataset.insert(ch);
         }
+        if sup.args.part_two {
+            priochr = '0';
+            nelves += 1;
+            if nelves == 3
+            {
+                nelves = 0;
+                let mut isect: HashSet<char> = HashSet::new();
+                for ch in prevdataset {
+                    if prevprevdataset.contains(&ch) {
+                        isect.insert(ch);
+                    }
+                }
+                for ch in dataset {
+                    if isect.contains(&ch) {
+                        priochr = ch;
+                    }
+                }
+                prevprevdataset = HashSet::new();
+                prevdataset = HashSet::new();
+            }
+            else
+            {
+                prevprevdataset = prevdataset;
+                prevdataset = dataset;
+            }
+            dataset = HashSet::new();
+        }
+        
         let priority = match priochr {
             'a'..='z' => priochr as u32 - 'a' as u32 + 1,
             'A'..='Z' => priochr as u32 - 'A' as u32 + 27,
+            '0' => 0,
             _ => panic!(),
         };
         score += priority;
+          
+        
     }
     println!("Score: {}", score);
     Ok(())
