31a93ef74ec68db95c68f3508c47b8278716244b
Alistair Bell <alistair@bellsonline.com>
2022-12-05 21:26:29 -0500

    Day 4.

---
+++ src/bin/4.rs
@@ -0,0 +1,48 @@
+use advent2022::Support;
+use std::io;
+
+fn main() -> io::Result <()> {    
+    let sup = Support::new()?;
+
+    let mut score: u32 = 0;
+    for line in sup.lines {
+        let line = line?;
+        // Split the line by commas.
+        let commas = line.split(",");
+        let mut limits: Vec<Vec<i32>> = vec![];
+        for c in commas {
+            let lim = c.split("-");
+            let mut lvec: Vec<i32> = vec![];
+            for l in lim {
+                let l: i32 = l.parse().unwrap();
+                lvec.push(l);
+            }
+            limits.push(lvec);
+        }
+        if sup.args.part_two {
+            if limits[0][0] <= limits[1][0] && limits[0][1] >= limits[1][0] {
+                score += 1;
+            }   
+            else if limits[0][0] <= limits[1][1] && limits[0][1] >= limits[1][1] {
+                score += 1;
+            }   
+            else if limits[1][0] <= limits[0][0] && limits[1][1] >= limits[0][0] {
+                score += 1;
+            }   
+            else if limits[1][0] <= limits[0][1] && limits[1][1] >= limits[0][1] {
+                score += 1;
+            }   
+        }
+        else
+        {
+           if limits[0][0] <= limits[1][0] && limits[0][1] >= limits[1][1] {
+                score += 1;
+            }   
+            else if limits[0][0] >= limits[1][0] && limits[0][1] <= limits[1][1] {
+                score += 1;
+            }   
+        }   
+    }
+    println!("Score: {}", score);
+    Ok(())
+}
